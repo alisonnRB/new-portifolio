e8f012ecff66874399455eb2ce4b5d2e8c8416b3
alisonnRB <alisonrbjs@gmail.com>
2024-04-24 12:51:32 -0300

    vercel analitics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Body from './components/body/index';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div className="App">
+      <Analytics />
       <Body />
     </div>
   );
